390eb1206cad7d07edf5692d9c25c6b7367036da
Javier Gombinsky <javier.gombinsky@gmail.com>
2024-03-11 19:45:29 +0100

    [CONFIG] change background colors

--- config/nvim/.config/nvim/init.lua
+++ config/nvim/.config/nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd 'hi Normal guibg=none ctermbg=none'
+vim.cmd 'hi LineNr guibg=none ctermbg=none'
+vim.cmd 'hi Folded guibg=none ctermbg=none'
+vim.cmd 'hi NonText guibg=none ctermbg=none'
+vim.cmd 'hi SpecialKey guibg=none ctermbg=none'
+vim.cmd 'hi VertSplit guibg=none ctermbg=none'
+vim.cmd 'hi SignColumn guibg=none ctermbg=none'
+vim.cmd 'hi EndOfBuffer guibg=none ctermbg=none'
+vim.cmd 'hi EndOfFile guibg=none ctermbg=none'
