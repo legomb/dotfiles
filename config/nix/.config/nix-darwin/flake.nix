d0f5c03a91697e9c7385b0135e5ddb3912cc77dc
Javier Gombinsky <javier.gombinsky@gmail.com>
2025-01-05 10:05:32 +0100

    feat: allow unfree

--- config/nix/.config/nix-darwin/flake.nix
+++ config/nix/.config/nix-darwin/flake.nix
@@ -10,6 +10,9 @@
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
+
+      nixpkgs.config.allowUnfree = true;
+
       # List packages installed in system profile. To search by name, run:
       # $ nix-env -qaP | grep wget
       environment.systemPackages =
