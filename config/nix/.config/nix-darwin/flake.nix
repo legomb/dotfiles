4c94b5bbe574f8915fd35842d2ac5ee22fc05489
Javier Gombinsky <javier.gombinsky@gmail.com>
2025-01-05 09:46:07 +0100

    chore: revert file

--- config/nix/.config/nix-darwin/flake.nix
+++ config/nix/.config/nix-darwin/flake.nix
@@ -1,32 +1,43 @@
 {
-  description = "Flake for macOS configuration";
+  description = "Custom nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nix-darwin, ... }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      configuration = { config, pkgs, ... }: {
-        # Compatibility, please read the changelog before changing.
-        # $ darwin-rebuild changelog
-        system.stateVersion = 5;
+  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
 
-        # The platform the configuration will be used on.
-        nixpkgs.hostPlatform = system;
-      };
-    in
-    {
-      darwinConfigurations = {
-        macbook = nix-darwin.lib.darwinSystem {
-          system = system;
-          modules = [ configuration ];
-        };
-      };
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
     };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#macbook
+    darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
 }
