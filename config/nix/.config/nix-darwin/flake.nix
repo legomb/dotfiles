70bd670ee4d5a084eff4e261acf7e426dea33560
Javier Gombinsky <javier.gombinsky@gmail.com>
2025-01-05 09:18:17 +0100

    fix: version must be an int

--- config/nix/.config/nix-darwin/flake.nix
+++ config/nix/.config/nix-darwin/flake.nix
@@ -15,7 +15,7 @@
       configuration = { config, pkgs, ... }: {
         # Compatibility, please read the changelog before changing.
         # $ darwin-rebuild changelog
-        system.stateVersion = "5";
+        system.stateVersion = 5;
 
         # The platform the configuration will be used on.
         nixpkgs.hostPlatform = system;
